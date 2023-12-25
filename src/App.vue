0982f724fd05bec18555a139effa545f8d1c5d19
naeem123650 <naeemmemon60650@gmail.com>
2023-12-25 19:01:51 +0530

    footer update

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <template v-if="siteUnderMaintenace">
-      <section class="bg-white dark:bg-gray-900 h-screen flex flex-col items-center justify-center">
-        <img src="@/assets/images/maintenance.gif" alt="Maintenance Image" class="mb-8 max-w-full h-auto" />
-
-        <div class="text-center">
-          <h4 class="mb-4 text-2xl tracking-tight font-extrabold lg:text-5xl text-primary-600 dark:text-primary-500">Under
-            Maintenance</h4>
-          <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">We're sorry for the
-            inconvenience.</p>
-          <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Our site is currently undergoing maintenance
-            to provide you with an even better experience. Please check back later.
-          </p>
-        </div>
-      </section>
+      <Maintenance /> 
   </template>
   <template v-else>
     <!-- navbar -->
@@ -90,6 +78,7 @@ import SkillSkeleton from '@/components/Skeletons/Skills/Index.vue';
 import Testimonials from '@/components/Testimonials/Index.vue';
 import MobileTabs from '@/components/MobileTabs/Index.vue';
 import FooterComp from '@/components/Footer/Index.vue';
+import Maintenance from '@/components/Maintenance/Index.vue';
 
 import { isLoading, personalInfo, experienceInfo, educationInfo, skillsInfo, testimonialInfo,siteUnderMaintenace,fetchPersonalInfo } from "@/vuetils/usePortfolio";
 import { onMounted } from 'vue';
--- src/components/Footer/Index.vue
+++ src/components/Footer/Index.vue
@@ -4,45 +4,30 @@
         <a href="#" class="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
           Naeem Valiyani
         </a>
-        <span class="block text-sm text-center text-gray-500 dark:text-gray-400">© 2021-2022 Landwind™. All
-          Rights Reserved. Built with <a href="https://flowbite.com"
+        <span class="block text-sm text-center text-gray-500 dark:text-gray-400">Made with ❤ <a href="https://flowbite.com"
             class="text-purple-600 hover:underline dark:text-purple-500">Flowbite</a> and <a
             href="https://tailwindcss.com" class="text-purple-600 hover:underline dark:text-purple-500">Tailwind CSS</a>.
         </span>
         <ul class="flex justify-center mt-5 space-x-5">
           <li>
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
-
-              <InlineSvg name="facebook" />
-
-            </a>
-          </li>
-          <li>
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+            <a href="https://www.instagram.com/naeem_memon_/" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
 
               <InlineSvg name="instagram" />
 
             </a>
           </li>
-          <li>
+          <!-- <li>
             <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
 
 
               <InlineSvg name="twitter" />
 
             </a>
-          </li>
+          </li> -->
           <li>
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+            <a href="https://github.com/naeem123650" target="_blank" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
 
               <InlineSvg name="github" />
-
-            </a>
-          </li>
-          <li>
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
-
-              <InlineSvg name="dribble" />
 
             </a>
           </li>

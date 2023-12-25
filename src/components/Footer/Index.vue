006ff51afd82fbc6385edbedcfc28a9eec23a2b9
naeem123650 <naeemmemon60650@gmail.com>
2023-12-25 14:48:40 +0530

    image deleted from footer

--- src/components/Footer/Index.vue
+++ src/components/Footer/Index.vue
@@ -2,7 +2,6 @@
     <div class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
       <div class="text-center">
         <a href="#" class="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="">
           Naeem Valiyani
         </a>
         <span class="block text-sm text-center text-gray-500 dark:text-gray-400">© 2021-2022 Landwind™. All
